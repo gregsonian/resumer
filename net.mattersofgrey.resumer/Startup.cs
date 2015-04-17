d8117582ad151b41aa611b1ad33777c7ddf75b4b
Gregory Schilsson <gschilsson@live.com>
2015-04-17 19:46:33 -0400

    resumer initial create

---
+++ net.mattersofgrey.resumer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(net.mattersofgrey.resumer.Startup))]
+namespace net.mattersofgrey.resumer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
